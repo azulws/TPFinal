f91e66b6863f4cf0f869630a64c2d7074445a4bf
azulws <39834637+azulws@users.noreply.github.com>
2022-10-18 19:21:26 -0300

    Remuneration Form

--- Views/home-keeper.php
+++ Views/home-keeper.php
@@ -1,15 +1,16 @@
 <main>
+<h3 align="center">Add renumeration:</h3>
+<div align="center">
+  <form action="<?php echo FRONT_ROOT."Keeper/Modify"?>" method="post">
+  <div class="form__group">
+    <input type="number" class="form__input" name="remuneration" placeholder="Remuneration" required>
+    <label for="renumeration" class="form__label">Insert Remuneration</label>
+    <input class="btn-desencriptar" type="submit" value="Confirm">
+  </div>
+</div>
+<br>
 <div align = "center">
     <h1>MENU KEEPER</h1>
-</div>
-<h3>Add renumeration:</h3>
-<div align="center">
-  <form align="center" action="<?php echo FRONT_ROOT."Keeper/Modify"?>" method="post">
-  <div class="formgroup">
-    <input type="text" class="forminput" name="remuneration" placeholder="Remuneration" required>
-    <label for="renumeration" class="form__label">Renumeration</label>
-    <input type="submit" value="Remuneration">
-  </div>
 </div>
 <div align ="center">
     <a class="btn-encriptar" href="<?php echo FRONT_ROOT . "Availability/ShowListView"?>">AVAILABILITY </a>
