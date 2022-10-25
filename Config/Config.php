bcc4ba021052d621e8a5ef5efc53b7c28934e13b
santilopezDev <santilopez_marcos@hotmail.com>
2022-10-25 19:24:52 -0300

    Conexión a BDD

--- Config/Config.php
+++ Config/Config.php
@@ -7,6 +7,12 @@ define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
 define("IMG_PATH", VIEWS_PATH . "img/");
+
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "file_upload_example");
+define("DB_USER", "root");
+define("DB_PASS", "");
 ?>
 
 
