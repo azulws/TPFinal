3c11b29925825193b6204b672cbd7af06ceab395
azulws <39834637+azulws@users.noreply.github.com>
2022-10-31 12:57:29 -0300

    Try-Catch Keeper

--- DAO/KeeperDAO.php
+++ DAO/KeeperDAO.php
@@ -12,80 +12,96 @@
         
         public function Add(Keeper $keeper)
         {
-            $query = "INSERT INTO ".$this->tableName." (idKeeper, firstName, lastName, userName, password) VALUES (:idKeeper, :firstName, :lastName, :userName, :password)";
+            try{
+                $query = "INSERT INTO ".$this->tableName." (idKeeper, firstName, lastName, userName, password) VALUES (:idKeeper, :firstName, :lastName, :userName, :password)";
 
-            $parameters["idKeeper"] =  $keeper->getIdKeeper();
-            $parameters["firstName"] = $keeper->getFirstName();
-            $parameters["lastName"] = $keeper->getLastName();
-            $parameters["userName"] = $keeper->getUserName();
-            $parameters["password"] = $keeper->getPassword();
-            $parameters["remuneration"] = $keeper->setRemuneration("0");          
+                $parameters["idKeeper"] =  $keeper->getIdKeeper();
+                $parameters["firstName"] = $keeper->getFirstName();
+                $parameters["lastName"] = $keeper->getLastName();
+                $parameters["userName"] = $keeper->getUserName();
+                $parameters["password"] = $keeper->getPassword();
+                $parameters["remuneration"] = $keeper->setRemuneration("0");          
 
-            $this->connection = Connection::GetInstance();
+                $this->connection = Connection::GetInstance();
 
-            $this->connection->ExecuteNonQuery($query, $parameters);
+                $this->connection->ExecuteNonQuery($query, $parameters);
+            }catch(Exception $ex){
+                throw $ex;
+            }
         }
 
         public function GetAll()
         {
-            $keeperList = array();
+            try{
+                $keeperList = array();
 
-            $query = "SELECT idKeeper, firstName, lastName, userName, 'password' FROM ".$this->tableName;
+                $query = "SELECT idKeeper, firstName, lastName, userName, 'password' FROM ".$this->tableName;
 
-            $this->connection = Connection::GetInstance();
+                $this->connection = Connection::GetInstance();
 
-            $result = $this->connection->Execute($query);
+                $result = $this->connection->Execute($query);
 
-            foreach($result as $row)
-            {
-                $keeper = new Keeper();
-                $keeper->setIdKeeper($row["idKeeper"]);
-                $keeper->setFirstName($row["firstName"]);
-                $keeper->setLastName($row["lastName"]);
-                $keeper->setUserName($row["userName"]);
-                $keeper->setPassword($row["password"]);
-                $keeper->setRemuneration($row["remuneration"]);
+                foreach($result as $row)
+                {
+                    $keeper = new Keeper();
+                    $keeper->setIdKeeper($row["idKeeper"]);
+                    $keeper->setFirstName($row["firstName"]);
+                    $keeper->setLastName($row["lastName"]);
+                    $keeper->setUserName($row["userName"]);
+                    $keeper->setPassword($row["password"]);
+                    $keeper->setRemuneration($row["remuneration"]);
 
-                array_push($keeperList, $keeper);
+                    array_push($keeperList, $keeper);
+                }
+
+                return $keeperList;
+            }catch(Exception $ex){
+                throw $ex;
             }
-
-            return $keeperList;
         }
 
         public function Remove($id)
         {            
-            $query = "DELETE FROM ".$this->tableName." WHERE (idKeeper = :idKeeper)";
+            try{
+                $query = "DELETE FROM ".$this->tableName." WHERE (idKeeper = :idKeeper)";
 
-            $parameters["idKeeper"] =  $idKeeper;
+                $parameters["idKeeper"] =  $idKeeper;
 
-            $this->connection = Connection::GetInstance();
+                $this->connection = Connection::GetInstance();
 
-            $this->connection->ExecuteNonQuery($query, $parameters);
+                $this->connection->ExecuteNonQuery($query, $parameters);
+            }catch(Exception $ex){
+                throw $ex;
+            }
         }        
 
         public function GetByUserName($userName)
         {
-            $user = null;
+            try{
+                $user = null;
 
-            $query = "CALL Users_GetByUserName(?)";
+                $query = "CALL Users_GetByUserName(?)";
 
-            $parameters["userName"] = $userName;
+                $parameters["userName"] = $userName;
 
-            $this->connection = Connection::GetInstance();
+                $this->connection = Connection::GetInstance();
 
-            $results = $this->connection->Execute($query, $parameters, QueryType::StoredProcedure);
+                $results = $this->connection->Execute($query, $parameters, QueryType::StoredProcedure);
 
-            foreach($results as $row)
-            {
-                $keeper = new Keeper();
-                $keeper->setIdKeeper($row["idKeeper"]);
-                $keeper->setFirstName($row["firstName"]);
-                $keeper->setLastName($row["lastName"]);
-                $keeper->setUserName($row["userName"]);
-                $keeper->setPassword($row["password"]);
+                foreach($results as $row)
+                {
+                    $keeper = new Keeper();
+                    $keeper->setIdKeeper($row["idKeeper"]);
+                    $keeper->setFirstName($row["firstName"]);
+                    $keeper->setLastName($row["lastName"]);
+                    $keeper->setUserName($row["userName"]);
+                    $keeper->setPassword($row["password"]);
+                }
+
+                return $keeper;
+            }catch(Exception $ex){
+                throw $ex;
             }
-
-            return $keeper;
         }  
     }
 ?>
