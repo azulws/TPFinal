625405f9996167cfa12da3fcefaaa9b777eb3c7b
Bruno Pruzsiani <brunopruzsi@gmail.com>
2022-11-25 18:33:38 -0300

    eliminado animacion de boton, require en msg

--- Views/chat.php
+++ Views/chat.php
@@ -36,7 +36,7 @@
         ?>       
         </section>                  
         
-        <input class="sendInput" type="text" name="msg">
+        <input class="sendInput" type="text" name="msg" require>
         <input type="hidden" name="idKeeper" value="<?php echo $idKeeper ?>">
         <input type="hidden" name="idOwner" value="<?php echo $idOwner ?>">
         <input type="hidden" name="isKeeper" value="<?php echo $isKeeper ?>">
